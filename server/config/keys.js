e513ac61dc66b8dba91906c0ec10e08d5140459e
Shivanshudeveloper <developer.shivanshu@gmail.com>
2022-08-27 11:55:07 +0530

    New Connection

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,6 +1,6 @@
 module.exports = {
   MongoURI:
-    "mongodb+srv://shrikantsahu2004:XHlhHi4k2TXUD6S0@cluster0.gtxqw.mongodb.net/?retryWrites=true&w=majority",
+    "mongodb+srv://admincyberclick:yDKlpmdAswUTXXtI@cluster0.br7zwbx.mongodb.net/?retryWrites=true&w=majority",
 };
 
 // module.exports = {
